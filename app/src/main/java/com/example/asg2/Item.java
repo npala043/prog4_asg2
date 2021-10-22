c51979cd7e0b4ca4d43963c934b95fb9e90c0ff2
Nahuel Paladino <43184105+npala043@users.noreply.github.com>
2021-10-22 15:47:27 -0600

    Created other starting files

---
+++ app/src/main/java/com/example/asg2/Item.java
@@ -0,0 +1,4 @@
+package com.example.asg2;
+
+public class Item {
+}
--- app/src/main/java/com/example/asg2/ItemPage.java
+++ app/src/main/java/com/example/asg2/ItemPage.java
@@ -5,12 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class ItemPage extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_item_page);
     }
 
     @Override
