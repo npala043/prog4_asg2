63ee9a0986eea422409fd79273283344dae89439
Nahuel Paladino <43184105+npala043@users.noreply.github.com>
2021-10-25 15:27:40 -0600

    finished generateListView

--- app/src/main/java/com/example/asg2/MainActivity.java
+++ app/src/main/java/com/example/asg2/MainActivity.java
@@ -4,6 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -11,10 +19,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // on MainActivity startup, read items.txt into ArrayList<Item> and call generateListView()
+        ArrayList<Item> itemList = new ArrayList<>(); // replace once readFile() is complete
+        generateListView(itemList);
+
     }
 
     @Override
     public void onClick(View v) {
 
     }
+
+    private void generateListView(ArrayList<Item> itemList) {
+        ListView mainList = (ListView) findViewById(R.id.mainList);
+        ItemsAdapter adapter = new ItemsAdapter(this, itemList);
+        mainList.setAdapter(adapter);
+    }
+
 }
