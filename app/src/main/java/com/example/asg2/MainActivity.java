71ad59e581eeba93300ed3987f5e4e3cc5b39bf1
PedroJanikian <43455987+PedroJanikian@users.noreply.github.com>
2021-10-26 15:25:23 -0600

    Added fileRead
    Used InputStream to get the raw file (through getResources) and used the Scanner to read the contents of the file.

--- app/src/main/java/com/example/asg2/MainActivity.java
+++ app/src/main/java/com/example/asg2/MainActivity.java
@@ -2,8 +2,20 @@ package com.example.asg2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -11,10 +23,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fileRead("items");
     }
 
     @Override
     public void onClick(View v) {
 
+    }
+
+    // Am I even doing this right?
+    public ArrayList<String> fileRead(String filePath) {
+        ArrayList<String> fileArr = new ArrayList<>();
+        try {
+            InputStream inputStream = getResources().openRawResource(getResources().getIdentifier(filePath, "raw", getPackageName()));
+            Scanner reader = new Scanner(inputStream);
+            Log.d("MainActivity - fileRead", ""+reader.hasNextLine());
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+                Log.i("MainActivity - fileRead",""+line);
+                fileArr.add(line);
+            }
+            reader.close();
+            inputStream.close();
+        } catch(IOException ignore) {
+            Log.e("MainActivity - fileRead","File " + filePath + " not found. Error in fileRead() method.");
+        }
+        Log.d("MainActivity", fileArr.get(0));
+        return fileArr;
     }
 }
