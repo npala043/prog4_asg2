8ad81257931fb3f9c8b9252e6ca051a64c46182d
PedroJanikian <pjani371@mtroyal.ca>
2021-10-26 17:04:50 -0600

    Merging Changes to Issue Branch (#15)
    * populated Item class
    
    * finished generateListView
    
    * Finished onclick handler (with dummy itemlist)
    
    * removed empty lines
    
    * onClick sends Item to next activity, cleanup and comments (#13)
    
    Co-authored-by: Nahuel Paladino <43184105+npala043@users.noreply.github.com>

--- app/src/main/java/com/example/asg2/Item.java
+++ app/src/main/java/com/example/asg2/Item.java
@@ -1,4 +1,40 @@
 package com.example.asg2;
 
-public class Item {
+import java.io.Serializable;
+
+public class Item implements Serializable {
+
+    private int id;
+    private String name;
+    private int quantity;
+    private double cost;
+    private int suppId;
+
+    public Item(int id, String name, int quantity, double cost, int suppId) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.cost = cost;
+        this.suppId = suppId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public int getSuppId() {
+        return suppId;
+    }
 }
--- app/src/main/java/com/example/asg2/MainActivity.java
+++ app/src/main/java/com/example/asg2/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.asg2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -23,7 +27,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        fileRead("items");
+
+        // on MainActivity startup, read items.txt into ArrayList<Item> and call generateListView()
+        ArrayList<Item> itemList = new ArrayList<>(); // replace once readFile() is complete
+
+        // dummy arraylist fill
+        for(int i = 0; i < 15; i++) {
+            Item item = new Item(i, "Item " + i, 0, 0, 0);
+            itemList.add(item);
+        }
+        generateListView(itemList);
     }
 
     @Override
@@ -31,8 +44,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
+    private void generateListView(ArrayList<Item> itemList) {
+        ListView mainList = findViewById(R.id.mainList);
+
+        // Facilitates custom creation of ListView from ArrayList
+        ItemsAdapter adapter = new ItemsAdapter(this, itemList);
+        mainList.setAdapter(adapter);
+
+        // Click listener for each item in ListView
+        // Original code from StackOverflow, refactored to match this project and converted to lambda expression
+        mainList.setOnItemClickListener((parent, view, position, id) -> {
+            Item item = adapter.getItem(position);
+
+            Bundle b = new Bundle();
+            b.putSerializable("item", item);
+
+            Intent intent = new Intent(MainActivity.this, ItemPage.class);
+            intent.putExtras(b);
+            startActivity(intent);
+        });
+    }
+
     /**
-     *
+     *  Can call like so: fileRead("items");
      * @param filePath: This is the file path without the '.txt' extension.
      *
      * @return ArrayList<String>: Returns an ArrayList of String;
